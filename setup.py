1cf29718ee53d8a290839d54ddf9a752bd6b3554
Russell Jurney <russell.jurney@gmail.com>
2013-09-30 18:19:23 -0700

    Trying to make pip install work.

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
     name='pyDruid',
-    version='0.1.0',
+    version='0.01.2',
     author='Deep Ganguli',
     author_email='deep@metamarkets.com',
     packages=['pydruid', 'pydruid.utils'],
     url='http://pypi.python.org/pypi/pyDruid/',
     license='LICENSE',
     description='Druid analytical data-store Python library',
-    long_description=open('README.md').read(),
+    long_description=open('README').read(),
     install_requires=[
         "pandas >= 0.12",
         "simplejson >= 3.3.0",
