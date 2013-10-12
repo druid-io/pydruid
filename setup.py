7a39a1aa66a4b3ffea3f374e0404da9127a33190
Russell Jurney <russell.jurney@gmail.com>
2013-10-11 17:03:46 -0700

    Removed pandas, matplotlib and scipy as deps.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyDruid',
-    version='0.1.7',
+    version='0.1.8',
     author='Deep Ganguli',
     author_email='deep@metamarkets.com',
     packages=['pydruid', 'pydruid.utils'],
@@ -11,9 +11,9 @@ setup(
     description='Druid analytical data-store Python library',
     long_description=open('README.txt').read(),
     install_requires=[
-        "pandas >= 0.12",
-        "simplejson >= 3.3.0",
-        "matplotlib >= 1.3.0",
-        "numpy >= 1.7.1"
+        #"pandas >= 0.12",
+        #"matplotlib >= 1.3.0",
+        #"numpy >= 1.7.1",
+        "simplejson >= 3.3.0"
     ],
 )
