d64a56b255c5f63a9e5539c56b5d4908d16e4f1b
Deep Ganguli <deep@metamarkets.com>
2014-03-07 11:03:01 -0800

    setup.py: upgrade version to 0.2.0, remove extraneous dependencies

--- setup.py
+++ setup.py
@@ -2,18 +2,15 @@ from setuptools import setup
 
 setup(
     name='pyDruid',
-    version='0.1.7',
+    version='0.2.0',
     author='Deep Ganguli',
     author_email='deep@metamarkets.com',
     packages=['pydruid', 'pydruid.utils'],
     url='http://pypi.python.org/pypi/pyDruid/',
     license='LICENSE',
-    description='Druid analytical data-store Python library',
-    long_description=open('README.txt').read(),
+    description='A Python connector for Druid.',
+    long_description='See https://github.com/metamx/pydruid for more information.',
     install_requires=[
-        "pandas >= 0.12",
         "simplejson >= 3.3.0",
-        "matplotlib >= 1.3.0",
-        "numpy >= 1.7.1"
     ],
 )
