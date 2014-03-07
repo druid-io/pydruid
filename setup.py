bc625b5acfa50f7f8d6090f22dba53dbac709488
Deep Ganguli <deep@metamarkets.com>
2014-03-07 11:32:41 -0800

    re-name pypi package from pyDruid -> pydruid

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='pyDruid',
+    name='pydruid',
     version='0.2.0',
     author='Deep Ganguli',
     author_email='deep@metamarkets.com',
     packages=['pydruid', 'pydruid.utils'],
-    url='http://pypi.python.org/pypi/pyDruid/',
+    url='http://pypi.python.org/pypi/pydruid/',
     license='LICENSE',
     description='A Python connector for Druid.',
     long_description='See https://github.com/metamx/pydruid for more information.',
