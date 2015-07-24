41ea841eb54f5b30776c6ba0bd7f414adcc002e4
Xavier Léauté <xavier@metamarkets.com>
2015-07-24 11:00:19 -0700

    update version to 0.2.1 and fix license string

--- setup.py
+++ setup.py
@@ -28,12 +28,12 @@ if sys.version_info < (2, 6):
 
 setup(
     name='pydruid',
-    version='0.2.0',
+    version='0.2.1',
     author='Deep Ganguli',
     author_email='deep@metamarkets.com',
     packages=['pydruid', 'pydruid.utils'],
     url='http://pypi.python.org/pypi/pydruid/',
-    license='LICENSE',
+    license='Apache License, Version 2.0',
     description='A Python connector for Druid.',
     long_description='See https://github.com/metamx/pydruid for more information.',
     install_requires=install_requires,
