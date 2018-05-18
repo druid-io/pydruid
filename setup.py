b136e15d197a036c6ca54155dad6a80f8e430dab
Maxime Beauchemin <maximebeauchemin@gmail.com>
2018-05-18 13:22:32 -0500

    0.4.3

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if sys.version_info < (2, 6):
 
 setup(
     name='pydruid',
-    version='0.4.2',
+    version='0.4.3',
     author='Druid Developers',
     author_email='druid-development@googlegroups.com',
     packages=['pydruid', 'pydruid.db', 'pydruid.utils'],
