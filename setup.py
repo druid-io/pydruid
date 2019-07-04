c6b5a0dc5444c3fbca97e729fc51ffe41578f8f1
Maxime Beauchemin <maximebeauchemin@gmail.com>
2019-07-03 22:54:41 -0700

    0.5.6

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with io.open('README.md', encoding='utf-8') as f:
 
 setup(
     name='pydruid',
-    version='0.5.5',
+    version='0.5.6',
     author='Druid Developers',
     author_email='druid-development@googlegroups.com',
     packages=['pydruid', 'pydruid.db', 'pydruid.utils'],
