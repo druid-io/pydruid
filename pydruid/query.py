d1ea36198f4db718a4de3b0540fdf55a2c8d52ba
SongYunSeop <dbstjq91@gmail.com>
2020-04-24 14:51:38 +0900

    Apply black

--- pydruid/query.py
+++ pydruid/query.py
@@ -513,7 +513,7 @@ class QueryBuilder(object):
             "metrics",
             "intervals",
             "limit",
-            "order"
+            "order",
         ]
         self.validate_query(query_type, valid_parts, args)
         return self.build_query(query_type, args)
