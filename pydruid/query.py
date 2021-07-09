434b9699e535289e5724bbb11cf5ae0d8bdbad42
Lavanya Pant <pantlavanya@gmail.com>
2021-07-10 00:34:25 +0530

    enable scan query support offset

--- pydruid/query.py
+++ pydruid/query.py
@@ -440,7 +440,7 @@ class QueryBuilder(object):
         query_type = 'scan'
         valid_parts = [
             'datasource', 'granularity', 'filter', 'dimensions', 'metrics',
-            'intervals', 'limit',
+            'intervals', 'limit', 'offset'
         ]
         self.validate_query(query_type, valid_parts, args)
         return self.build_query(query_type, args)
