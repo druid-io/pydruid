22ea292b799320261835a8b2184cf2092658bf0b
Usiel Riedl <usiel.riedl@automattic.com>
2022-08-09 11:22:37 +0800

    Moves DruidDialect tests into single file

--- tests/db/test_druid_dialect.py
+++ tests/db/test_druid_dialect.py
@@ -58,6 +58,24 @@ class DruidDialectTestSuite(unittest.TestCase):
 
         self.assertListEqual(result, expected)
 
+    @patch("pydruid.db.api.Connection")
+    def test_do_ping_success(self, connection_mock):
+        connection_mock.execute.return_value = [1]
+
+        result = self.dialect.do_ping(connection_mock)
+
+        # asserts the ping executes with a raw string
+        connection_mock.execute.assert_called_once_with("SELECT 1")
+        self.assertTrue(result)
+
+    @patch("pydruid.db.api.Connection")
+    def test_do_ping_with_exception(self, connection_mock):
+        connection_mock.execute.side_effect = Exception("Something's wrong :(")
+
+        result = self.dialect.do_ping(connection_mock)
+
+        self.assertFalse(result)
+
 
 if __name__ == "__main__":
     unittest.main()
